cbf441bb58ff9c91fbe8724d67a37a8932e789c1
Yogesh <yogeshsharma999011@gmail.com>
2026-07-17 10:29:34 +0530

    reading data

--- data_script.py
+++ data_script.py
@@ -3,5 +3,9 @@ import pandas as pd
 # read data from an Excel file
 df = pd.read_excel('C:\\Users\\hp\\OneDrive\\Desktop\\pandas\\SampleSuperstore.xlsx')
 print(df)
-print(df)
-
+# read data from an csv file 
+# df = pd.read_csv('C:\\Users\\hp\\OneDrive\\Desktop\\pandas\\SampleSuperstore.csv')
+# print(df)
+# read data from a json file
+js = pd.read_json("C:\\Users\\hp\\OneDrive\\Desktop\\pandas\\sample_Data.json")
+print(js)    
